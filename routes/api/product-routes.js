c6438e7cf55486eba30420c6ee34afe4c58d0554
jarenella <jamesa122333@gmail.com>
2022-11-01 12:18:07 -0700

    product routes now working but still need to give associated tags and categories

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  const allProducts = await Product.findAll();
+  // include its associated Category and Tag data
+  const categoryData = await Category.findAll();
+  const tagData = await Tag.findAll();
+  //send response
+  res.status(200).json(allProducts);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  const product = await Product.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+  // include its associated Category and Tag data
+  //send response
+  res.status(200).json(product);
 });
 
 // create new product
@@ -91,6 +103,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  //send response
+  res.send("delete request successfully recieved");
 });
 
 module.exports = router;
